ae2cec93cc218d6e22a6f2802b73138f717f1359
nerseshepoyan <nerses.hepoyan@student.rau.am>
2021-06-08 00:40:24 +0400

    data base , gradaran

---
+++ Source.cpp
@@ -0,0 +1,38 @@
+
+#include <sql.h>// main SQLAPI++ header
+#include <sqlext.h>
+#include <sqloledb.h>
+#include <sqltypes.h>
+#include <sqlucode.h>
+#include <sqlucode.h>
+#include <Sql_1.h>
+#include <winsqlite/winsqlite3.h>
+#include <winsqlite/winsqlite3ext.h>
+#include <iostream>
+#include <string>
+
+
+int main(int argc, char** argv)
+{
+    sqlite3* DB;
+    std::string sql = "CREATE TABLE PERSON("
+        "ID INT PRIMARY KEY     NOT NULL, "
+        "NAME           TEXT    NOT NULL, "
+        "SURNAME          TEXT     NOT NULL, "
+        "AGE            INT     NOT NULL, "
+        "ADDRESS        CHAR(50), "
+        "SALARY         REAL );";
+    int exit = 0;
+    exit = sqlite3_open("example.db", &DB);
+    char* messaggeError;
+    exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messaggeError);
+
+    if (exit != SQLITE_OK) {
+        std::cerr << "Error Create Table" << std::endl;
+        sqlite3_free(messaggeError);
+    }
+    else
+        std::cout << "Table created Successfully" << std::endl;
+    sqlite3_close(DB);
+    return (0);
+}
